99b45ff6312b4a90ca0f6aac10056db19dca404e
Madusha Jayawardhana <madusha.jayawardhana@ifsworld.com>
2019-11-12 17:44:35 +0530

    monthly spending

--- BudgetManager/Form1.Designer.cs
+++ BudgetManager/Form1.Designer.cs
@@ -31,7 +31,9 @@
             this.btnManageTransaction = new System.Windows.Forms.Button();
             this.btnBudgetMng = new System.Windows.Forms.Button();
             this.btnCategoryMgt = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.expenseGroup = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.expenseGroup.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnManageTransaction
@@ -64,26 +66,39 @@
             this.btnCategoryMgt.UseVisualStyleBackColor = true;
             this.btnCategoryMgt.Click += new System.EventHandler(this.categoryMgtButtonClick);
             // 
-            // groupBox1
+            // expenseGroup
             // 
-            this.groupBox1.Location = new System.Drawing.Point(177, 46);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(648, 542);
-            this.groupBox1.TabIndex = 8;
-            this.groupBox1.TabStop = false;
+            this.expenseGroup.Controls.Add(this.label1);
+            this.expenseGroup.Location = new System.Drawing.Point(207, 26);
+            this.expenseGroup.Name = "expenseGroup";
+            this.expenseGroup.Size = new System.Drawing.Size(683, 604);
+            this.expenseGroup.TabIndex = 3;
+            this.expenseGroup.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Monthly Spending";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(862, 633);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(938, 692);
+            this.Controls.Add(this.expenseGroup);
             this.Controls.Add(this.btnCategoryMgt);
             this.Controls.Add(this.btnBudgetMng);
             this.Controls.Add(this.btnManageTransaction);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.loadForm);
+            this.expenseGroup.ResumeLayout(false);
+            this.expenseGroup.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -93,7 +108,8 @@
         private System.Windows.Forms.Button btnManageTransaction;
         private System.Windows.Forms.Button btnBudgetMng;
         private System.Windows.Forms.Button btnCategoryMgt;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox expenseGroup;
+        private System.Windows.Forms.Label label1;
     }
 }
 
